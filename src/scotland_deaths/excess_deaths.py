1a770380df816604677346be0266da5af6322dbf
Richard Lyon <richlyon@mac.com>
2021-09-25 10:02:53 +0100

    multipanel plot

--- src/scotland_deaths/excess_deaths.py
+++ src/scotland_deaths/excess_deaths.py
@@ -4,7 +4,6 @@
 """
 
 import matplotlib.pyplot as plt
-import pandas as pd
 
 from scotland_deaths import OUT_DIR
 from scotland_deaths.covid_deaths import CovidDeaths
@@ -60,32 +59,23 @@ def plot_total_vs_actual_deaths(ax, covid_deaths: CovidDeaths):
     ax.spines["bottom"].set_visible(False)
 
 
-def plot_covid_vs_noncovid_deaths(ax, df_deaths):
-    # compute "all locations"
-    df_n = df_deaths.groupby(level=[0, 1], axis=1).sum()
+def plot_covid_vs_noncovid_deaths(ax, covid_deaths: CovidDeaths):
 
-    causes = list(df_n["2021"].columns)
-    non_covid_causes = [c for c in causes if c != "COVID-19"]
-
-    df_data = pd.DataFrame(columns=["Covid", "non-Covid"])
-    df_data["Covid"] = df_n["2021"]["COVID-19"] - df_n["(2015-2019)"]["COVID-19"]
-    df_data["non-Covid"] = df_n["2021"][non_covid_causes].sum(axis=1) - df_n[
-        "(2015-2019)"
-    ][non_covid_causes].sum(axis=1)
+    df = covid_deaths.get_covid_non_covid_excess_deaths()
 
     ax.plot(
-        df_data.index,
-        df_data["non-Covid"],
+        df.index,
+        df["non-Covid"],
         color="tab:blue",
         # marker="o",
         linewidth=3,
         label="Cancer, Heart Disease, Stroke, Dementia, Altheimers, Respiratory, Other",
     )
-    ax.plot(df_data.index, df_data["Covid"], color="grey", label="Covid")
+    ax.plot(df.index, df["Covid"], color="grey", label="Covid")
     ax.fill_between(
-        df_data.index,
-        df_data["Covid"],
-        df_data["non-Covid"],
+        df.index,
+        df["Covid"],
+        df["non-Covid"],
         color="tab:blue",
         alpha=0.1,
     )
@@ -100,43 +90,7 @@ def plot_covid_vs_noncovid_deaths(ax, df_deaths):
     ax.spines["bottom"].set_visible(False)
 
 
-# def plot_geriatric_vs_nongeriatric_deaths(ax, df_deaths):
-#     # compute "all locations"
-#     df_n = df_deaths.groupby(level=[0, 1], axis=1).sum()
-#     causes = list(df_n["2021"].columns)
-#     non_geriatric_causes = ["Cancer", "Other"]
-#     geriatric_causes = [c for c in causes if c not in non_geriatric_causes]
-#     print(geriatric_causes)
-#
-#     df_data = pd.DataFrame(columns=["Geriatric", "non Geriatric"])
-#     df_data["Geriatric"] = df_n["2021"][geriatric_causes].sum(axis=1) - df_n[
-#         "(2015-2019)"
-#     ][geriatric_causes].sum(axis=1)
-#     df_data["non Geriatric"] = df_n["2021"][non_geriatric_causes].sum(axis=1) - df_n[
-#         "(2015-2019)"
-#     ][non_geriatric_causes].sum(axis=1)
-#
-#     ax.plot(df_data.index, df_data["Geriatric"], color="tab:red", label="Geriatric")
-#     ax.plot(
-#         df_data.index,
-#         df_data["non Geriatric"],
-#         color="tab:green",
-#         # marker="o",
-#         label="non Geriatric",
-#     )
-#     ax.fill_between(
-#         df_data.index,
-#         df_data["Geriatric"],
-#         df_data["non Geriatric"],
-#         color="grey",
-#         alpha=0.2,
-#     )
-#     ax.title.set_text("Geriatric vs non-Geriatric excess deaths, Scotland 2021")
-#     ax.legend(loc="upper right")
-# plt.axhline(y=0, color="grey")
-
-
-if __name__ == "__main__":
+def plot_two_panel_excess_deaths():
     covid_deaths = CovidDeaths(week_no=37)
 
     fig, [ax1, ax2] = plt.subplots(2, 1)
@@ -145,13 +99,58 @@ if __name__ == "__main__":
     fig.suptitle("Why are Governments not talking about non-Covid excess deaths?")
 
     plot_total_vs_actual_deaths(ax1, covid_deaths)
-    plot_covid_vs_noncovid_deaths(ax2, covid_deaths.get_excess_deaths())
-    # plot_geriatric_vs_nongeriatric_deaths(ax3, c.excess_death_df)
+    plot_covid_vs_noncovid_deaths(ax2, covid_deaths)
 
-    # non_geriatric_causes = ["Cancer", "Other"]
-    # geriatric_causes = [c for c in causes if c not in non_geriatric_causes]
-
-    # df_data.plot()
-    # print(df_n["2021"].to_markdown())
     plt.savefig(OUT_DIR / "Excess Deaths Scotland 2021.png")
     plt.show()
+
+
+def plot_respiratory_illness_by_location():
+    covid_deaths = CovidDeaths(week_no=37)
+    df = covid_deaths.get_excess_deaths()
+    df = df["2021"] - df["(2015-2019)"]
+
+    fig, [(ax1, ax2), (ax3, ax4), (ax5, ax6)] = plt.subplots(3, 2)
+    fig.suptitle(
+        "Excess death by cause and location, Scotland 2021 (SOURCE: National Records of Scotland)"
+    )
+    fig.set_size_inches(16, 12)
+    fig.patch.set_facecolor("white")
+
+    ax1.annotate(
+        "richardlyon.substack.com",
+        (0, 0),
+        (50, 50),
+        xycoords="figure points",
+        textcoords="offset pixels",
+        va="top",
+        color="grey",
+    )
+
+    causes = [
+        (ax1, "COVID-19"),
+        (ax2, "Cancer"),
+        (ax3, "Respiratory"),
+        (ax4, "Circulatory (heart disease and stroke)"),
+        (ax5, "Other"),
+        (ax6, "Dementia / Alzheimers"),
+    ]
+    for ax, cause in causes:
+        ax.title.set_text(cause)
+        ax.spines["top"].set_visible(False)
+        ax.spines["right"].set_visible(False)
+        ax.spines["left"].set_visible(False)
+        ax.spines["bottom"].set_visible(False)
+
+        for location in df[cause].columns:
+            ax.bar(df.index, df[cause][location], width=5, label=location)
+            if ax == ax1:
+                ax.legend(loc=("upper right"))
+
+    plt.savefig(OUT_DIR / "Excess death by cause and location, Scotland 2021.png")
+    plt.show()
+
+
+if __name__ == "__main__":
+    # plot_two_panel_excess_deaths()
+    plot_respiratory_illness_by_location()
