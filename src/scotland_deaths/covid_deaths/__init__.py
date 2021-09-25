c8b1ed705d7bff63e049c760aa6a940283f63d9a
Richard Lyon <richlyon@mac.com>
2021-09-25 17:04:19 +0100

    refactor class

---
+++ src/scotland_deaths/covid_deaths/__init__.py
@@ -0,0 +1,1 @@
+from .covid_deaths import CovidDeaths
--- src/scotland_deaths/make_charts.py
+++ src/scotland_deaths/make_charts.py
@@ -9,6 +9,8 @@ from scotland_deaths import OUT_DIR
 from scotland_deaths.covid_deaths import CovidDeaths
 
 
+# my_cmap = sns.light_palette()
+
 def plot_total_vs_actual_deaths(ax, covid_deaths: CovidDeaths):
 
     df_deaths = covid_deaths.get_all_deaths(resample=True)
@@ -106,21 +108,33 @@ def plot_two_panel_excess_deaths():
 
 
 def plot_respiratory_illness_by_location():
+    plt.rcParams['axes.facecolor'] = (0.95, 0.95, 0.95)
+
     covid_deaths = CovidDeaths(week_no=37)
     df = covid_deaths.get_excess_deaths()
     df = df["2021"] - df["(2015-2019)"]
+    df = df.resample("D").interpolate(method="quadratic")
 
     fig, [(ax1, ax2), (ax3, ax4), (ax5, ax6)] = plt.subplots(3, 2)
     fig.suptitle(
-        "Excess death by cause and location, Scotland 2021 (SOURCE: National Records of Scotland)"
+        "Excess death by cause and location, Scotland 2021 vs (2015-2019)"
     )
     fig.set_size_inches(16, 12)
-    fig.patch.set_facecolor("white")
+    # fig.patch.set_facecolor("lightgrey")
 
     ax1.annotate(
         "richardlyon.substack.com",
         (0, 0),
         (50, 50),
+        xycoords="figure points",
+        textcoords="offset pixels",
+        va="top",
+        color="grey",
+    )
+    ax1.annotate(
+        "SOURCE: National Records of Scotland",
+        (0, 0),
+        (1250, 50),
         xycoords="figure points",
         textcoords="offset pixels",
         va="top",
@@ -136,16 +150,34 @@ def plot_respiratory_illness_by_location():
         (ax6, "Dementia / Alzheimers"),
     ]
     for ax, cause in causes:
+
+        cause_df = df[cause]
+        total_df = cause_df.sum(axis=1)
+
+        # split dataframe df into negative only and positive only values
+        df_neg, df_pos = cause_df.clip(upper=0), cause_df.clip(lower=0)
+        # stacked area plot of positive values
+        df_pos.plot.area(ax=ax, stacked=True, linewidth=0.0, legend=False)
+        # reset the color cycle
+        ax.set_prop_cycle(None)
+        # stacked area plot of negative values, prepend column names with '_' such that they don't appear in the legend
+        df_neg.rename(columns=lambda x: "_" + x).plot.area(
+            ax=ax, stacked=True, linewidth=0.0, legend=False
+        )
+        # rescale the y axis
+        # ax.set_ylim([df_neg.sum(axis=1).min(), df_pos.sum(axis=1).max()])
+        ax.set_ylim([-150, 400])
+
+        total_df.plot(ax=ax, color="black", linewidth=1, label="Total")
+
+        if ax == ax1:
+            ax.legend(loc=("upper right"))
         ax.title.set_text(cause)
-        ax.spines["top"].set_visible(False)
+        # ax.spines["top"].set_visible(False)
         ax.spines["right"].set_visible(False)
         ax.spines["left"].set_visible(False)
         ax.spines["bottom"].set_visible(False)
 
-        for location in df[cause].columns:
-            ax.bar(df.index, df[cause][location], width=5, label=location)
-            if ax == ax1:
-                ax.legend(loc=("upper right"))
 
     plt.savefig(OUT_DIR / "Excess death by cause and location, Scotland 2021.png")
     plt.show()
